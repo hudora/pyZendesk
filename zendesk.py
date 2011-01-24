3fb4ff9deacef569b3b4e59c79d8202359b467ff
Christian Klein <c.klein@hudora.de>
2011-01-24 10:05:03 +0100

    Updated code

--- zendesk.py
+++ zendesk.py
@@ -3,59 +3,71 @@
 zendesk.py - Zendesk.com automation
 
 Written by Yurii Zolotko in behalf of HUDORA.
-Copyright (c) 2010 HUDORA. All rights reserved.
+Updated by Christian Klein
+Copyright (c) 2010, 2011 HUDORA. All rights reserved.
+
+Configuration via config.py or environment:
+ZENDESK_DOMAIN
+ZENDESK_REMOTE_AUTH_TOKEN
+ZENDESK_CREDENTIALS
 """
 
-import os
-import urllib2
-from urllib import basejoin, urlencode
-from time import time
+import huTools.http
+import time
+import urlparse
+import xml.etree.ElementTree as ET
+from urllib import urlencode
 from hashlib import md5
 
-__all__ = ('create_user', 'sign_link', 'create_link')
 
-DEBUG_LEVEL = 1
+try:
+    import config
+    get_config = lambda key: getattr(config, key, None)
+except ImportError:
+    import os
+    get_config = os.getenv
+
 
 def create_user(name, email):
     """Create new user in Zendesk"""
+
+    root = ET.Element('user')
+    ET.SubElement(root, 'name').text = name
+    ET.SubElement(root, 'email').text = email
+
+    headers = {'Content-Type': 'application/xml; charset=utf-8'}
+    url = urlparse.urljoin('https://%s.zendesk.com' % get_config('ZENDESK_DOMAIN'), '/users.xml')
     
-    url = basejoin('https://%s.zendesk.com' % os.getenv('ZENDESK_DOMAIN', ''), 'users.xml')
-    auth_handler = urllib2.HTTPBasicAuthHandler()
-    username, password = os.getenv('ZENDESK_CREDENTIALS', ':').split(':', 2)
-    auth_handler.add_password(realm='Web Password', uri=url, user=username, passwd=password)
-
-    class SetXmlContentType(urllib2.BaseHandler):
-        def http_request(self, req):
-            req.add_header('Content-Type', 'application/xml; charset=utf-8')
-            return req
-
-    opener = urllib2.build_opener(SetXmlContentType(), auth_handler,
-        urllib2.HTTPHandler(debuglevel=DEBUG_LEVEL))
-    data = '<user><name>%s</name><email>%s</email></user>' % (name, email)
-    response = opener.open(url, data=data)
-    return response.info().getheader('Location')
+    status, headers, content = huTools.http.fetch(url, content=ET.tostring(root),
+                                                  headers=headers,
+                                                  credentials=get_config('ZENDESK_CREDENTIALS'))
+    return headers.get('Location')
 
 
-def sign_link(name, email, timestamp):
-    """Sign remote auth link"""
-    token = name + email + os.environ.get('ZENDESK_REMOTE_AUTH_TOKEN', '') + str(timestamp)
-    return basejoin('https://%s.zendesk.com' % os.getenv('ZENDESK_DOMAIN'), '/access/remote/?' + urlencode({
-        'name': name,
-        'email': email,
-        'timestamp': timestamp,
-        'hash': md5(token).digest().encode('hex')}))
+def get_signature(name, email, timestamp):
+    """Create signature for a remote auth link"""
+
+    tmp = ''.join((name, email, get_config('ZENDESK_REMOTE_AUTH_TOKEN'), timestamp))
+    return md5(tmp).hexdigest()
 
 
-def create_link(name, email):
+def create_link(name, email, timestamp=None, return_to=None):
     """Create remote auth link"""
-    return sign_link(name, email, int(time()))
+
+    if timestamp is None:
+        timestamp = str(int(time.time()))
+
+    signature = get_signature(name, email, timestamp)
+    params = {'name': name, 'email': email, 'timestamp': timestamp, 'hash': signature}
+    if return_to:
+        params['return_to'] = return_to
+
+    link = urlparse.urlunparse(('https',
+                                '%s.zendesk.com' % get_config('ZENDESK_DOMAIN'),
+                                '/access/remote/',
+                                '',
+                                urlencode(params),
+                                ''))
+    return link
 
 
-if __name__ == '__main__':
-    from sys import argv
-    if argv[1] == 'createuser':
-        print create_user(argv[2], argv[3])
-    elif argv[1] == 'createlnk':
-        print create_link(argv[2], argv[3])
-    elif argv[1] == 'signlnk':
-        print sign_link(argv[2], argv[3], argv[4])
